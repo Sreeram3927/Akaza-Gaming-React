b10f2ce906dd20fd7edbd2df69fdf5cf4df7f4f6
Sreeram3927 <sreeram292004@gmail.com>
2023-10-06 17:56:16 +0530

    add logo to website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './index.css';
+import img from './assets/logo.jpg';
 
 function App() {
   return (
     <div className="container">
+
       <div className="top-bar">
         Akaza Gaming
       </div>
+
+      <div className="content">
+        <img src={img} alt="My Logo"/>
+      </div>
+
     </div>
   );
 }
